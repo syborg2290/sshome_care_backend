8b9e6f2919d3bbb7f667d8b6248a0dac2f166918
keptoon <launcherstechinfo@gmail.com>
2020-10-12 13:19:04 +0530

    server 1 commited

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const socketio = require("socket.io");
-const http = require("http");
 const app = express();
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
@@ -19,10 +17,6 @@ app.use(helmet());
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-const server = http.createServer(app);
-const io = socketio(server);
-const connectionDb = require("./database");
-
 //Test Routes
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -33,51 +27,12 @@ app.use("/api/item", require("./routes/item"));
 app.use("/api/imageUpload", require("./routes/uploadMedia"));
 app.use("/api/deleteImage", require("./routes/deleteMedia"));
 
-// let interval;
 
-// io.on("connection", (socket) => {
-//   console.log("New client connected");
-//   if (interval) {
-//     clearInterval(interval);
-//   }
-//   interval = setInterval(() => getItems(socket), 1000);
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//     connectionDb.end();
-//     clearInterval(interval);
-//   });
-// });
-
-// const getItems = (socket) => {
-//   var sql = "SELECT * FROM item";
-//   connectionDb.query(sql, function (err, results) {
-//     if (err) {
-//       throw err;
-//     }
-//     socket.emit("fetchItems", results);
-//   });
-// };
-
-io.on("connection", function (socket) {
-  socket.on("fetchItems", () => {
-    var sql = "SELECT * FROM item";
-    connectionDb.query(sql, function (err, results) {
-      if (err) {
-        throw err;
-      }
-      io.emit("messageFromServer", results);
-    });
-  });
-});
+const PORT = process.env.PORT || 5000;
 
 
-const port1 = process.env.PORT || 9095;
-const port2 = process.env.PORT || 6065;
-
-app.listen(port1, () => {
+app.listen(PORT, () => {
   console.log("App running on port 5000");
 });
 
-server.listen(port2, () => {
-  console.log("Server running on port 4000");
-});
+
